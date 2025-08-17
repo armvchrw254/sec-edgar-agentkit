670a84156483b1e61f60bd66871ff27af2b872cc
Stefano Amorelli <stefano@amorelli.tech>
2025-08-17 16:04:04 +0300

    fix(langgraph): update jest config to use ts-jest preset instead of missing Nx preset

--- integrations/langgraph/jest.config.cjs
+++ integrations/langgraph/jest.config.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-  preset: '../../jest.preset.cjs',
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: {
-        jsx: 'react',
-        esModuleInterop: true
-      }
-    }]
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
     '!src/**/index.ts'
-  ]
+  ],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true
+      }
+    }]
+  }
 };
